561a44451615c3778f6ce48fd599647d848af2ec
Manoj-V07 <manoj.v2024cse@sece.ac.in>
2025-12-04 10:39:30 +0530

    Task1 Login Page

---
+++ LMS Task1/Login/script.js
@@ -0,0 +1,8 @@
+function login(){
+    const email = document.getElementById("email").value;
+    const pwd = document.getElementById("password").value;
+
+    if(email !== "" && pwd !== ""){
+        alert("Login Successfull");
+    }
+}
